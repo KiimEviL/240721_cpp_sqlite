f99352adce091b959e7c0db136a3d7705dfb27e0
KiimEviL <kimasia3000@gmail.com>
2024-08-07 13:06:29 +0700

    Добавьте файлы проекта.

---
+++ 240721_cpp_sqlite/240721_cpp_sqlite.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+#include "sqlite/sqlite3.h"
+using namespace std;
+
+int main() {
+	char* err;
+	sqlite3* db;
+	sqlite3_stmt* s;
+	sqlite3_open("DB.db", &db);
+
+	string sql = "CREATE TABLE IF NOT EXISTS GRADES("
+		"ID INTEGER PRIMARY KEY AUTOINCREMENT, "
+		"NAME      TEXT NOT NULL, "
+		"LNAME     TEXT NOT NULL, "
+		"AGE       INT  NOT NULL, "
+		"ADDRESS   CHAR(50), "
+		"GRADE     CHAR(1) );";
+	int rc = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS GRADES(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME      TEXT NOT NULL, LNAME     TEXT NOT NULL, AGE       INT  NOT NULL, ADDRESS   CHAR(50), GRADE     CHAR(1) );", NULL, NULL, &err);
+	if (rc != SQLITE_OK) {
+		cerr << "Creating table error: " << err;
+	}
+	return 0;
+}
